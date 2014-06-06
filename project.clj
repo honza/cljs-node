aca03e924d7652b3fd3107f111a46fd7ac995871
Honza Pokorny <me@honza.ca>
2014-06-06 20:06:11 +0200

    Bump version, 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-node/lein-template "0.1.0"
+(defproject cljs-node/lein-template "0.1.1"
   :description "cljs-node template"
   :license "BSD"
   :url "https://github.com/honza/cljs-node"
